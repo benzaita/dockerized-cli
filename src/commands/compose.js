4da4521cee48a16d2546926383824d2662fc20ef
ItamarB <itamar.ben-zaken@klarna.com>
2018-08-19 17:06:11 +0200

    fixes `builder compose stop name`

--- src/commands/compose.js
+++ src/commands/compose.js
@@ -1,34 +1,26 @@
-const child_process = require("child_process")
-const path = require("path")
-const debug = require("debug")("builder:compose")
-const findBaseDir = require("../utils/find-base-dir")
-const readConfig = require("../utils/read-config")
-const prettifyErrors = require("../utils/prettify-errors")
+const findBaseDir = require('../utils/find-base-dir')
+const readConfig = require('../utils/read-config')
+const prettifyErrors = require('../utils/prettify-errors')
+const DockerCompose = require('../utils/docker-compose')
 
 function execDockerComposeCommand(command, baseDir, dockerComposeFile) {
-  const args = ["-f", path.resolve(baseDir, dockerComposeFile), command]
-
-  debug(`running: docker-compose ${args.join(" ")}`)
-  const child = child_process.spawn("docker-compose", args, {
-    stdio: "inherit"
-  })
-
-  child.on("exit", code => {
-    debug(`child process exited with code ${code}. propagating`)
-    process.exit(code)
+  DockerCompose.spawn({
+    baseDir,
+    dockerComposeFile,
+    dockderComposeArgs: command,
   })
 }
 
 module.exports = {
-  command: "compose",
-  desc: "run a docker-compose command",
+  command: 'compose',
+  desc: 'run a docker-compose command',
   builder: yargs => yargs,
   handler: prettifyErrors(async function exec(argv) {
-    const command = process.argv.slice(3).join(" ")
+    const command = process.argv.slice(3)
     const baseDir = await findBaseDir()
     const config = readConfig(baseDir)
     const dockerComposeFile = config.composeFile
 
     execDockerComposeCommand(command, baseDir, dockerComposeFile)
-  })
+  }),
 }
