a3c1f3d953acef7a7fef2b8c21b0fbe3aeb764dd
benzaita <benzaita@gmail.com>
2019-07-20 22:55:28 +0200

    assert init fails when already initialized

--- src/acceptance-tests/init.spec.ts
+++ src/acceptance-tests/init.spec.ts
@@ -3,19 +3,20 @@ import execDockerizedSync from "../utils/exec-dockerized-sync";
 import deltree from "deltree"
 import { EOL } from 'os';
 
+let workdir: string
+
+beforeEach(() => {
+    workdir = fs.mkdtempSync('dockerized-test-')
+})
+
+afterEach(async () => {
+    await deltree(workdir)
+})
+
 describe('when not initialized yet', () => {
-    let workdir: string
-
-    beforeEach(() => {
-        workdir = fs.mkdtempSync('dockerized-test-')
-    })
-
-    afterEach(async () => {
-        await deltree(workdir)
-    })
-
     it('should initialize', () => {
         const { stdout, stderr, status } = execDockerizedSync('init', [], { cwd: workdir })
+        expect(status).toBe(0)
         expect(stdout).toEqual('')
         expect(stderr.split(EOL)).toEqual([
             'created .dockerized/docker-compose.dockerized.yml',
@@ -23,6 +24,21 @@ describe('when not initialized yet', () => {
             'hint: edit .dockerized/Dockerfile.dockerized to set up your container',    
             ''
         ])
-        expect(status).toBe(0)
+    })
+})
+
+describe('when already initialized', () => {
+    beforeEach(() => {
+        execDockerizedSync('init', [], { cwd: workdir })
+    })
+
+    it('should fail', () => {
+        const { stdout, stderr, status } = execDockerizedSync('init', [], { cwd: workdir })
+        expect(status).toBe(1)
+        expect(stdout).toEqual('')
+        expect(stderr.split(EOL)).toEqual([
+            'already initialized',    
+            ''
+        ])
     })
 })
