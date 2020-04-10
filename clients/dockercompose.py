99ed57375e638a3a5ea37e1a96af4183bd9f08e1
ItamarB <itamar.ben-zaken@klarna.com>
2020-04-10 14:30:06 +0200

    wip

--- clients/dockercompose.py
+++ clients/dockercompose.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from typing import List
 
+# Why not use the Docker Compose API directly?
+# Because [it is not officially supported](https://github.com/docker/compose/issues/4542#issuecomment-283191533)
+
 
 class DockerCompose:
     def run(self, stdout, stderr, composefile: Path, working_dir: Path, bind_dir: Path, command: List[str]):
