e511fa57d83bc9aa2860751caa33094d8042f940
ItamarB <itamar.ben-zaken@klarna.com>
2020-04-11 13:05:43 +0200

    wip

--- test_dockerized.py
+++ test_dockerized.py
@@ -1,3 +1,4 @@
+import subprocess
 import os
 import shutil
 import unittest
@@ -5,14 +6,14 @@ import tempfile
 
 
 class TestEndToEnd(unittest.TestCase):
-    temp_dir: str
+    working_dir: str
 
     def setUp(self) -> None:
-        self.temp_dir = tempfile.mkdtemp()
-        os.chdir(self.temp_dir)
+        self.working_dir = tempfile.mkdtemp()
+        os.chdir(self.working_dir)
 
     def tearDown(self) -> None:
-        shutil.rmtree(self.temp_dir)
+        shutil.rmtree(self.working_dir)
 
     def test_init_succeeds(self):
         self.assertDockerized(
@@ -55,18 +56,34 @@ class TestEndToEnd(unittest.TestCase):
             expected_stderr=b'something err\n',
         )
 
-    def assertDockerized(self, command, expected_exit_code, expected_stdout, expected_stderr):
+    def test_exec_runs_in_container_built_by_Dockerfile(self):
+        self.assertDockerized(
+            fixture_name='with_foo_env_var',
+            command='exec echo $FOO',
+            expected_exit_code=0,
+            expected_stdout=b'1\n',
+            expected_stderr=b'',
+        )
+
+    def assertDockerized(self, command, expected_exit_code, expected_stdout, expected_stderr, fixture_name=None):
+        if fixture_name is not None:
+            self._copy_fixture_to_working_dir(fixture_name)
         exit_code, stdout, stderr = self.run_dockerized(command)
         self.assertEqual(expected_stderr, stderr)
         self.assertEqual(expected_stdout, stdout)
         self.assertEqual(expected_exit_code, exit_code)
 
     def run_dockerized(self, cmd_line):
-        import subprocess
-        import os
-        dockerized = os.path.dirname(os.path.realpath(__file__)) + '/dockerized.py'
-        process = subprocess.run(f"{dockerized} {cmd_line}", cwd=self.temp_dir, shell=True, capture_output=True)
+        this_file_path = os.path.dirname(os.path.realpath(__file__))
+        dockerized = this_file_path + '/dockerized.py'
+        process = subprocess.run(f"{dockerized} {cmd_line}", cwd=self.working_dir, shell=True, capture_output=True)
         return process.returncode, process.stdout, process.stderr
+
+    def _copy_fixture_to_working_dir(self, fixture_name):
+        shutil.rmtree(self.working_dir)
+
+        this_file_path = os.path.dirname(os.path.realpath(__file__))
+        shutil.copytree(this_file_path + '/fixtures/' + fixture_name, self.working_dir)
 
 
 if __name__ == '__main__':
