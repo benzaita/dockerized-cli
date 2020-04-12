27923842ef8e8f964de4010c5306cabb42d6e19e
ItamarB <itamar.ben-zaken@klarna.com>
2020-04-12 14:35:33 +0200

    test_exec_runs_from_sub_dir

--- adapters/environment.py
+++ adapters/environment.py
@@ -4,7 +4,14 @@ from pathlib import Path
 
 class Environment:
     def get_project_dir(self):
-        return self.get_working_dir()
+        curr_dir = self.get_working_dir()
+        while curr_dir != Path.root and not curr_dir.joinpath('.dockerized').is_dir():
+            curr_dir = curr_dir.parent
+
+        if curr_dir == Path.root:
+            return None
+        else:
+            return curr_dir
 
     def get_working_dir(self):
         return Path(os.getcwd())
