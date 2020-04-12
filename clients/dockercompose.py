15449145295ab15762aebbacd7def48d41ecfa46
ItamarB <itamar.ben-zaken@klarna.com>
2020-04-12 13:48:12 +0200

    wip

--- clients/dockercompose.py
+++ clients/dockercompose.py
@@ -1,24 +1,32 @@
 import sys
 import subprocess
 from pathlib import Path
+import logging
 
-# Why not use the Docker Compose API directly?
-# Because [it is not officially supported](https://github.com/docker/compose/issues/4542#issuecomment-283191533)
+logger = logging.getLogger(__name__)
 
 
 class DockerCompose:
     def run(self, composefile: Path, working_dir: Path, bind_dir: Path, command: str):
+        # Why not use the Docker Compose API directly?
+        # Because [it is not officially supported](https://github.com/docker/compose/issues/4542#issuecomment-283191533)
+
+        args = [
+            'docker-compose',
+            '-f',
+            composefile,
+            'run',
+            '--rm',
+            'dockerized',
+            command
+        ]
+        logger.info(f"Running: {args}")
         try:
-            args = [
-                'docker-compose',
-                '-f',
-                composefile,
-                'run',
-                '--rm',
-                'dockerized',
-                command
-            ]
             process = subprocess.Popen(args, stdout=sys.stdout, stderr=sys.stderr)
-            return process.wait()
+            exit_code = process.wait()
         except Exception as e:
+            logger.error(f"Raised exception: {e}")
             raise e
+
+        logger.info(f"Finished with exit-code {exit_code}")
+        return exit_code
--- dockerized.py
+++ dockerized.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
+import logging
+import os
 from ui import cli
 
 if __name__ == '__main__':
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "CRITICAL"))
     cli.main()
