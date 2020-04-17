45f87c77ce62eea9f8d7c9cf7db4e890ebef6bcd
ItamarB <itamar.ben-zaken@klarna.com>
2020-04-17 23:03:50 +0200

    Bump version: 0.9.5 → 0.9.6

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import json
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.9.5"
+version = "0.9.6"
 
 setup(
     name="dockerized",
@@ -15,7 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/benzaita/dockerized-cli",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
