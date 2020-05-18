dcc023fe966fb92b6b03b173810b0d250e37e7c1
ItamarB <itamar.ben-zaken@klarna.com>
2020-05-18 16:24:50 +0200

    better regex

--- test_dockerized.py
+++ test_dockerized.py
@@ -226,7 +226,7 @@ class EndToEndTest(AbstractEndToEndTest):
             expected_stdout_regex=r'.*',
             expected_stderr_regex=''.join([
                 r'#\d \[\d/\d\] RUN echo "long operation"\n',  # this step
-                r'(#\d pulling .*?\n)?',  # might be pulled (or already available locally)
+                r'(#\d pulling .*?\n)*',  # might be pulled (or already available locally)
                 r'#\d CACHED',  # anyway - it should be cached
             ]),
         )
