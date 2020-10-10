0527f879ffb00e026c22cc51f24be126ba5a9507
benzaita <benzaita@gmail.com>
2020-10-10 18:45:30 +0200

    introduce DockerizedConfig class

--- dockerized/core/commands/dockercomposecommand.py
+++ dockerized/core/commands/dockercomposecommand.py
@@ -1,11 +1,28 @@
 import logging
 from pathlib import Path
+from typing import List
 from dockerized.adapters.dockercompose import DockerCompose
 from dockerized.adapters.environment import Environment
 from dockerized.core.commands.errors import CommandError
 from dockerized.core.project import Project
 
 logger = logging.getLogger(__name__)
+
+class DockerizedConfig:
+    project_dir: Path
+    config_dict: dict
+
+    def __init__(self, project_dir: Path, config_dict: dict):
+        self.project_dir = project_dir
+        self.config_dict = config_dict
+    
+    def compose_files(self) -> List[Path]:
+        to_path_relative_to_project = lambda s: self.project_dir.joinpath('.dockerized').joinpath(s)
+        
+        return list(map(to_path_relative_to_project, self.config_dict['compose_files']))
+
+    def service_name(self) -> str:
+        return self.config_dict['service_name']
 
 
 class DockerComposeCommand:
@@ -20,10 +37,15 @@ class DockerComposeCommand:
         if self.project_dir is None:
             raise CommandError("Not inside a Dockerized project directory. Did you run 'dockerized init'?")
 
+        self.config = DockerizedConfig(self.project_dir, {
+            'compose_files': ['./docker-compose.dockerized.yml'],
+            'service_name': 'dockerized'
+        })
+
         self.docker_compose = docker_compose or DockerCompose(
-            composefiles=[self.project_dir.joinpath('.dockerized').joinpath('docker-compose.dockerized.yml')],
+            composefiles=self.config.compose_files(),
             project_dir=self.project_dir,
-            service_name='dockerized'
+            service_name=self.config.service_name()
         )
 
         self.project = Project(project_dir=self.project_dir, env=self.env, docker_compose=self.docker_compose)
